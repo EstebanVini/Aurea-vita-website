4b343e757c8864acc01776ce943b4aff68aaa97d
Esteban Viniegra <esteban@eviniegra.software>
2026-06-12 16:21:58 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+        allowedHosts: ["dev-aureavita.eviniegra.qzz.io", "dev-aureavita.consorciorazo.com"]
+        }
 });
